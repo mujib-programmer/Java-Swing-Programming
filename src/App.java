76df00f9dbab68e2dbc429e55f65ce549d9272c3
PHPGeek Programmer <phpgeek.programmer@gmail.com>
2015-02-09 10:10:00 +0700

    introduction to swing

---
+++ src/App.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Hello World");
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600, 500);
+
+	}
+
+}
