d4284ced4dee2394397deb94d550153a5c4a09b9
PHPGeek Programmer <phpgeek.programmer@gmail.com>
2015-02-10 09:47:57 +0700

    add List Boxes

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -6,109 +6,135 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 public class FormPanel extends JPanel {
-	
+
 	private JLabel nameLabel;
 	private JLabel occupationLabel;
 	private JTextField nameField;
 	private JTextField occupationField;
-	private JButton okBtn; 
+	private JButton okBtn;
 	private FormListener formListener;
-	
+	private JList ageList;
+
 	public void setFormListener(FormListener formListener) {
 		this.formListener = formListener;
 	}
 
 	public FormPanel() {
 		Dimension dim = getPreferredSize();
-		
+
 		dim.width = 256;
 		setPreferredSize(dim);
-		
+
 		nameLabel = new JLabel("Name: ");
 		occupationLabel = new JLabel("Occupation: ");
 		nameField = new JTextField(10);
 		occupationField = new JTextField(10);
+		ageList = new JList();
+
+		DefaultListModel ageModel = new DefaultListModel();
+		ageModel.addElement("Under 18");
+		ageModel.addElement("18 to 65");
+		ageModel.addElement("65 or over");
+
+		ageList.setModel(ageModel);
 		
+		ageList.setPreferredSize(new Dimension(110, 70));
+		ageList.setBorder(BorderFactory.createEtchedBorder());
+
 		okBtn = new JButton("OK");
-		
-		okBtn.addActionListener(new ActionListener(){
+
+		okBtn.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
 				String name = nameField.getText();
 				String occupation = occupationField.getText();
+				String ageCat = (String) ageList.getSelectedValue();
 				
+				// tes hasil pemilihan umur
+				System.out.println(ageCat);
+
 				FormEvent ev = new FormEvent(this, name, occupation);
-				
-				if(formListener != null) {
+
+				if (formListener != null) {
 					formListener.formEventOccurred(ev);
 				}
-			}			
-	
-			
-			
+			}
+
 		});
 
 		Border innerBorder = BorderFactory.createTitledBorder("Add Person");
 		Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
 		setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
-		
+
 		setLayout(new GridBagLayout());
-		
+
 		GridBagConstraints gc = new GridBagConstraints();
-		
-		/////// First Row /////////////////////	
+
+		// ///// First Row /////////////////////
 
 		gc.weightx = 1;
 		gc.weighty = 0.1;
-		
+
 		gc.gridx = 0;
 		gc.gridy = 0;
 		gc.fill = GridBagConstraints.NONE;
 		gc.anchor = GridBagConstraints.LINE_END;
 		gc.insets = new Insets(0, 0, 0, 5);
 		add(nameLabel, gc);
-		
+
 		gc.gridx = 1;
 		gc.gridy = 0;
 		gc.anchor = GridBagConstraints.LINE_START;
 		gc.insets = new Insets(0, 0, 0, 0);
 		add(nameField, gc);
-		
-		/////// Second Row /////////////////////
-		
+
+		// ///// Second Row /////////////////////
+
 		gc.weightx = 1;
 		gc.weighty = 0.1;
-		
+
 		gc.gridx = 0;
 		gc.gridy = 1;
 		gc.anchor = GridBagConstraints.LINE_END;
 		gc.insets = new Insets(0, 0, 0, 5);
 		add(occupationLabel, gc);
-		
+
 		gc.gridx = 1;
 		gc.gridy = 1;
 		gc.anchor = GridBagConstraints.LINE_START;
 		gc.insets = new Insets(0, 0, 0, 0);
 		add(occupationField, gc);
-		
-		/////// Third Row /////////////////////
-		
+
+		// ///// Third Row /////////////////////
+
 		gc.weightx = 1;
-		gc.weighty = 2.0;
-		
+		gc.weighty = 0.2;
+
 		gc.gridx = 1;
 		gc.gridy = 2;
 		gc.anchor = GridBagConstraints.FIRST_LINE_START;
 		gc.insets = new Insets(0, 0, 0, 0);
+		add(ageList, gc);
+
+		// ///// Forth Row /////////////////////
+
+		gc.weightx = 1;
+		gc.weighty = 2.0;
+
+		gc.gridx = 1;
+		gc.gridy = 3;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
 		add(okBtn, gc);
-		
-		
+
 	}
 }
