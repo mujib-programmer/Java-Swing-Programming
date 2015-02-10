c449411237f54521acca04ece953fcc6406513e8
PHPGeek Programmer <phpgeek.programmer@gmail.com>
2015-02-10 10:00:32 +0700

    add menus

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,6 +4,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JTextArea;
 
 
@@ -24,6 +27,8 @@ public class MainFrame extends JFrame {
 		toolbar = new Toolbar();
 		textPanel = new TextPanel();
 		formPanel = new FormPanel();
+		
+		setJMenuBar(createMenuBar());
 		
 		// aktifkan event pada toolbar agar bisa bekerja dengan objek textPanel
 		toolbar.setStringListener(new StringListener() {
@@ -66,5 +71,31 @@ public class MainFrame extends JFrame {
 		// tampilkan MainFrame
 		setVisible(true);
 		
+	}
+	
+	private JMenuBar createMenuBar() {
+		JMenuBar menuBar =  new JMenuBar();
+		
+		JMenu fileMenu = new JMenu("File");
+		
+		JMenuItem exportDataItem = new JMenuItem("Export Data...");
+		JMenuItem importDataItem = new JMenuItem("Import Data...");
+		JMenuItem exitItem = new JMenuItem("Exit");
+		
+		fileMenu.add(exportDataItem);
+		fileMenu.add(importDataItem);
+		fileMenu.addSeparator();
+		fileMenu.add(exitItem);
+		
+		JMenu windowMenu =  new JMenu("Window");
+		JMenu showMenu = new JMenu("Show");
+		JMenuItem showFormItem = new JMenuItem("Person Form");
+		showMenu.add(showFormItem);
+		windowMenu.add(showMenu);
+		
+		menuBar.add(fileMenu);
+		menuBar.add(windowMenu);
+		
+		return menuBar;
 	}
 }
