f38db793831c66ed8a8aed19f800033414f9cd1d
PHPGeek Programmer <phpgeek.programmer@gmail.com>
2015-02-11 10:24:13 +0700

    using serialization to saving object to files

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import model.Database;
@@ -79,6 +81,16 @@ public class Controller {
 		
 		db.add(person);
 
+	}
+	
+	public void saveToFile(File file) throws IOException {
+		db.saveToFile(file);
+		
+	}
+	
+	public void loadFromFile(File file) throws IOException {
+		db.loadFromFile(file);
+		
 	}
 
 }
--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,8 +1,10 @@
 package gui;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBoxMenuItem;
@@ -19,9 +21,8 @@ import com.sun.glass.events.KeyEvent;
 
 import controller.Controller;
 
-
 public class MainFrame extends JFrame {
-	
+
 	// deklarasi komponen-komponen yang akan ditampilkan
 	private Toolbar toolbar;
 	private TextPanel textPanel;
@@ -32,134 +33,151 @@ public class MainFrame extends JFrame {
 
 	public MainFrame() {
 		super("Hello World");
-		
+
 		// layout menggunakan BorderLayout
 		setLayout(new BorderLayout());
-		
+
 		// inisiasi komponen
 		toolbar = new Toolbar();
 		textPanel = new TextPanel();
 		formPanel = new FormPanel();
 		tablePanel = new TablePanel();
-		
+
 		controller = new Controller();
-		
+
 		tablePanel.setData(controller.getPeople());
-		
-		fileChooser =  new JFileChooser();
+
+		fileChooser = new JFileChooser();
 		fileChooser.addChoosableFileFilter(new PersonFileFilter());
-		
+
 		setJMenuBar(createMenuBar());
-		
+
 		// aktifkan event pada toolbar agar bisa bekerja dengan objek textPanel
 		toolbar.setStringListener(new StringListener() {
 
 			public void textEmitted(String text) {
 				// TODO Auto-generated method stub
 				textPanel.appendText(text);
-				
+
 			}
-			
-		});	
-		
-		formPanel.setFormListener(new FormListener(){
-			public void formEventOccurred(FormEvent e) {		
+
+		});
+
+		formPanel.setFormListener(new FormListener() {
+			public void formEventOccurred(FormEvent e) {
 				controller.addPerson(e);
 				tablePanel.refresh();
-				
+
 			}
 		});
-		
+
 		// tambahkan komponen ke MainFrame
 		// secara default JFame sudah memiliki ContentPane sendiri
 		// jadi bisa langsung ditambahkan
 		add(formPanel, BorderLayout.WEST);
 		add(toolbar, BorderLayout.NORTH);
-		add(tablePanel, BorderLayout.CENTER);		
-		
+		add(tablePanel, BorderLayout.CENTER);
+
 		// atur ukuran frame
 		setMinimumSize(new Dimension(500, 400));
 		setSize(600, 500);
-		
+
 		// tutup frame ketika ikon x diklik
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		// tampilkan MainFrame
 		setVisible(true);
-		
+
 	}
-	
+
 	private JMenuBar createMenuBar() {
-		JMenuBar menuBar =  new JMenuBar();
-		
+		JMenuBar menuBar = new JMenuBar();
+
 		JMenu fileMenu = new JMenu("File");
-		
+
 		JMenuItem exportDataItem = new JMenuItem("Export Data...");
 		JMenuItem importDataItem = new JMenuItem("Import Data...");
 		JMenuItem exitItem = new JMenuItem("Exit");
-		
+
 		fileMenu.add(exportDataItem);
 		fileMenu.add(importDataItem);
 		fileMenu.addSeparator();
 		fileMenu.add(exitItem);
-		
-		JMenu windowMenu =  new JMenu("Window");
+
+		JMenu windowMenu = new JMenu("Window");
 		JMenu showMenu = new JMenu("Show");
-		
+
 		JCheckBoxMenuItem showFormItem = new JCheckBoxMenuItem("Person Form");
 		showFormItem.setSelected(true);
-		
+
 		showMenu.add(showFormItem);
 		windowMenu.add(showMenu);
-		
+
 		menuBar.add(fileMenu);
 		menuBar.add(windowMenu);
-		
-		showFormItem.addActionListener(new ActionListener(){
+
+		showFormItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ev) {
 				JCheckBoxMenuItem menuItem = (JCheckBoxMenuItem) ev.getSource();
-				
+
 				formPanel.setVisible(menuItem.isSelected());
 			}
-			
+
 		});
-		
+
 		fileMenu.setMnemonic(KeyEvent.VK_F);
 		exitItem.setMnemonic(KeyEvent.VK_X);
-		
-		exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
-		
-		importDataItem.addActionListener(new ActionListener(){
+
+		exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,
+				ActionEvent.CTRL_MASK));
+
+		importDataItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (fileChooser.showOpenDialog(MainFrame.this) == JFileChooser.APPROVE_OPTION) {
-					System.out.println(fileChooser.getSelectedFile());
+					try {
+						controller.loadFromFile(fileChooser.getSelectedFile());
+						tablePanel.refresh();
+						
+					} catch (IOException e1) {
+						JOptionPane.showMessageDialog(MainFrame.this,
+								"Could not load data from file", "Error",
+								JOptionPane.ERROR_MESSAGE);
+					}
+
 				}
 			}
-			
+
 		});
-		
-		exportDataItem.addActionListener(new ActionListener(){
+
+		exportDataItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (fileChooser.showSaveDialog(MainFrame.this) == JFileChooser.APPROVE_OPTION) {
-					System.out.println(fileChooser.getSelectedFile());
+					try {
+						controller.saveToFile(fileChooser.getSelectedFile());
+						
+					} catch (IOException e1) {
+						JOptionPane.showMessageDialog(MainFrame.this,
+								"Could not save data to file", "Error",
+								JOptionPane.ERROR_MESSAGE);
+					}
 				}
 			}
-			
+
 		});
-		
-		exitItem.addActionListener(new ActionListener(){
+
+		exitItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int action = JOptionPane.showConfirmDialog(MainFrame.this
-						, "Do you really want to exit the application?"
-						, "Confirm Exit", JOptionPane.OK_CANCEL_OPTION);
-				
+				int action = JOptionPane.showConfirmDialog(MainFrame.this,
+						"Do you really want to exit the application?",
+						"Confirm Exit", JOptionPane.OK_CANCEL_OPTION);
+
 				if (action == JOptionPane.OK_OPTION) {
-					System.exit(0);				
+					System.exit(0);
 				}
 			}
-			
+
 		});
-		
+
 		return menuBar;
 	}
 }
